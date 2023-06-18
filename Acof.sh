69297958cdbc0bfe9dc408a434cff0cea6fbefae
GameMeter <134675517+GameMeter@users.noreply.github.com>
2023-06-18 14:18:45 +0000

    Add files via upload

---
+++ Acof.sh
@@ -0,0 +1,30 @@
+# Configuration Script #
+#
+#
+#
+# Setting up an enviroment key so you can see icons in the dolphin file manager
+
+cd $HOME
+mkdir Changed\ Files
+sudo mv /etc/environment $HOME/Changed\ Files
+sudo mv $HOME/XMDE-UFS/ftgc/environment /etc/environment
+
+# Applying the XMonad.hs to play
+
+cd $HOME
+mkdir $HOME/.xmonad
+rm $HOME/.xmonad/xmonad.hs
+# ^ If the file had been created automatically
+mv $HOME/XMDE-UFS/ftgc/xmonad.hs $HOME/.xmonad/
+
+# Putting up wallpaper
+
+cd $HOME
+mkdir wallpaper
+mv $HOME/XMDE-UFS/wallpaper/*.* wallpaper/
+
+# Setting up Polybar 
+
+mkdir $HOME/.config
+mkdir $HOME/.config/polybar
+mv $HOME/XMDE-UFS/ftgc/config.ini $HOME/.config/polybar/
---
+++ Capps.sh
@@ -0,0 +1,19 @@
+# The Applications installer script #
+#
+# Installing Paru
+
+cd $HOME
+pacman -Sy git
+git clone https://aur.archlinux.org/paru.git
+cd $HOME/paru
+makepkg -si
+
+cd $HOME
+
+# Installing Main Repo Packages
+
+paru -S xmonad xmonad-contrib polybar vim emacs fish terminator dolphin qt5ct papirus-icon-theme lxappearance
+
+# Installing AUR Packages
+#
+paru -S orchis-theme-git timeshift-bin
